b7d51c1ab5a55f6c8a42566a950ce57d00091eb5
elevatexfitness <eamorally@lions.net>
2024-10-29 16:26:05 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script for form submission
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Thank you for reaching out to us!");
+});
